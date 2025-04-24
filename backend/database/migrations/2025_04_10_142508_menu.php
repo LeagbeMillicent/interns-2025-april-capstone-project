5afe517a3e302e36e12a83eaa5e63ea6dac9a918
Miss Leagbe <leagbemillicent@gmail.com>
2025-04-24 12:27:38 +0000

    add FK to menu and order_item table

--- backend/database/migrations/2025_04_10_142508_menu.php
+++ backend/database/migrations/2025_04_10_142508_menu.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->decimal('price', 8, 2);
             $table->string('image')->nullable();
             $table->text('description')->nullable();
+            $table->foreignId('category_id')->nullable()->on('categories')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
